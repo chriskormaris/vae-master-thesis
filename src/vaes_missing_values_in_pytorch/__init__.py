f3db5ebca9a4cf635a0695075db76a7eac814754
Christos Kormaris <chriskormaris@gmail.com>
2023-01-25 00:36:56 +0200

    Create and use more `__init__.py` files

---
+++ src/vaes_missing_values_in_pytorch/__init__.py
@@ -0,0 +1,7 @@
+from src.vaes_missing_values_in_pytorch.binarized_mnist import binarized_mnist
+from src.vaes_missing_values_in_pytorch.cifar10 import cifar10
+from src.vaes_missing_values_in_pytorch.mnist import mnist
+from src.vaes_missing_values_in_pytorch.movielens import movielens
+from src.vaes_missing_values_in_pytorch.omniglot import omniglot
+from src.vaes_missing_values_in_pytorch.orl_faces import orl_faces
+from src.vaes_missing_values_in_pytorch.yale_faces import yale_faces
