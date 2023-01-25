ee45c2e891e1105dc04948a60a97461f89c8d655
Christos Kormaris <chriskormaris@gmail.com>
2023-01-25 23:17:56 +0200

    Move the global variable inside the main
    Also, add imports in the `__init__.py` of the package `src.vaes_in_pytorch`

--- src/vaes_in_pytorch/__init__.py
+++ src/vaes_in_pytorch/__init__.py
@@ -1,1 +1,6 @@
-
+from src.vaes_in_pytorch.binarized_mnist import binarized_mnist
+from src.vaes_in_pytorch.cifar10 import cifar10
+from src.vaes_in_pytorch.mnist import mnist
+from src.vaes_in_pytorch.omniglot import omniglot
+from src.vaes_in_pytorch.orl_faces import orl_faces
+from src.vaes_in_pytorch.yale_faces import yale_faces
